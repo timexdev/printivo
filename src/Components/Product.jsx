60f5348926cded63a1a545bee3481bc4369dcd87
Peter Ajala <ajalapeter1@gmail.com>
2023-05-18 12:47:23 -0700

    added more products

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -52,6 +52,50 @@ const Product = () => {
                 </div> 
             </div>
         </div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12 col-md-3 mb-3'>
+                <div className="card shadow">
+                    <img style={{height:'250px'}} src="https://printivo.s3-us-west-2.amazonaws.com/files/category_thumbnails/1456215784_12.png" className="card-img-top" alt="Landscape ID"/>
+                    <div className="card-body">
+                        <h6 className="card-title fw-bold">Landscape ID Cards</h6>
+                        <p style={{fontSize:'12px'}} className="card-text fw-bold text-secondary">STARTING AT <br /> <span className='fw-bold fs-5 text-black'>#1,800</span> per 1</p>
+                        <a style={{backgroundColor:'rgb(215, 127, 129)', color:'white'}} href="#" className="btn fw-semibold">Browse Landscape ID... <i class="bi bi-arrow-right-short"></i> </a>
+                    </div>
+                </div>
+                </div>
+                <div className='col-12 col-md-3 mb-3'>
+                <div className="card shadow">
+                    <img style={{height:'250px'}} src="https://printivo.s3-us-west-2.amazonaws.com/files/category_thumbnails/1439790190_postcard.jpg" className="card-img-top" alt="Postcards"/>
+                    <div className="card-body">
+                        <h6 className="card-title fw-bold">Postcards</h6>
+                        <p style={{fontSize:'12px'}} className="card-text fw-bold text-secondary">STARTING AT <br /> <span className='fw-bold fs-5 text-black'>#11,000</span> per 50</p>
+                        <a style={{backgroundColor:'rgb(215, 127, 129)', color:'white'}} href="#" className="btn fw-semibold">Browse Postcards... <i class="bi bi-arrow-right-short"></i></a>
+                    </div>
+                </div>
+                </div>
+                <div className='col-12 col-md-3 mb-3'>
+                <div className="card shadow">
+                    <img style={{height:'250px'}} src="https://printivo.s3-us-west-2.amazonaws.com/files/category_thumbnails/1446183154_big%20rollup%20banner.png" className="card-img-top" alt="Roll up banners"/>
+                    <div className="card-body">
+                        <h6 className="card-title fw-bold">Roll Up Banners</h6>
+                        <p style={{fontSize:'12px'}} className="card-text fw-bold text-secondary">STARTING AT <br /> <span className='fw-bold fs-5 text-black'>#38,000</span> per 1</p>
+                        <a style={{backgroundColor:'rgb(215, 127, 129)', color:'white'}} href="#" className="btn fw-semibold">Browse Roll Up Ba... <i class="bi bi-arrow-right-short"></i></a>
+                    </div>
+                </div>
+                </div>
+                <div className='col-12 col-md-3 mb-3'>
+                <div className="card shadow">
+                    <img style={{height:'250px'}} src="https://printivo.s3-us-west-2.amazonaws.com/files/category_thumbnails/1661456358_social-media--16.png" alt='mugs'/>
+                    <div className="card-body">
+                        <h6 className="card-title fw-bold">Simple Mugs</h6>
+                        <p style={{fontSize:'12px'}} className="card-text fw-bold text-secondary">STARTING AT <br /> <span className='fw-bold fs-5 text-black'>#3,499</span> per 1</p>
+                        <a style={{backgroundColor:'rgb(215, 127, 129)', color:'white'}} href="#" className="btn fw-semibold">Browse Simple Mugs... <i class="bi bi-arrow-right-short"></i></a>
+                    </div>
+                </div>
+                </div> 
+            </div>
+        </div>
         
     
     </>
